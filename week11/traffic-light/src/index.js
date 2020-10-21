7d0896b9e1d39f3ac6d28198eea3b5ab919853d5
licop <licop@users.noreply.github.com>
2020-10-21 18:34:43 +0800

    promise异步学习

---
+++ week11/traffic-light/src/index.js
@@ -0,0 +1,19 @@
+// 红绿灯
+// function sleep(dur) {
+//   return new Promise(function(resolve) {
+//     setTimeout(resolve, dur)
+//   })
+// }
+
+// async function changeColor(dur, color) {
+//   document.getElementById('#traffic-light').background = color;
+//   await sleep(dur)
+// }
+
+// async function main() {
+//   while(true) {
+//     await changeColor(3000, "green");
+//     await changeColor(1000, "yellow");
+//     await changeColor(2000, "red");
+//   } 
+// }
